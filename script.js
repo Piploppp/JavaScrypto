4b82518458844feec1fddc16e575378b76a028d2
Piploppp <elchorizo2343@gmail.com>
2022-05-24 22:15:16 -0500

    Primera Versión

---
+++ script.js
@@ -0,0 +1,99 @@
+
+let js = "amazing";
+//console.log(40 + 8 + 23 - 10); 
+
+console.log("Jonas");
+console.log(23);
+
+let firstName = "Matilda";
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let jonas_matilda = "JM";
+let $function = 27;
+
+let person = "jonas"
+let PI = 3.1416;
+
+let myFirstJob = "Coder"
+let myCurrentJob = "Investor"
+
+let job1 = "programmer";
+let job2 = "teacher";
+
+console.log(myFirstJob)
+console.log(myCurrentJob)
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+//console.log(typeof true);
+console.log(typeof javascriptIsFun);
+//console.log(typeof 23);
+//console.log(typeof "Jonas");
+
+javascriptIsFun = "YES!";
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(typeof year);
+
+console.log(typeof null);
+
+ 
+let age = 30;
+age = 31;
+
+const birthYear = 1991;
+// birthYear = 1990;
+// const job;
+
+var  job = "programmer";
+job = "teacher"
+let lastName = "Schmedtmann";
+lastName = "Scaletta";
+console.log(lastName);
+
+
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+console.log(ageJonas, ageSarah);
+
+console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
+// 2 ** 3 means 2 to the power of 3 = 2* 2 * 2
+
+const firstNamex = "Jonas";
+console.log(firstName + " " + lastName);
+
+//Assignment operators
+let x = 10 + 5; // 15
+x += 10; // x = x + 10 = 25
+x *= 4; // x = x * 4 = 100
+x++; // x = x + 1
+x--;
+x--;
+console.log(x);
+
+// Comparison operators 
+console.log(ageJonas > ageSarah); // >, <, >=, <=
+console.log(ageSarah >= 18);
+
+const isFullAge = ageSarah >= 18;
+
+console.log(now - 1991 > now - 2018)
+
+
+let nowx = 2037;
+const ageJonasx = now - 1991;
+const ageSarahx = now - 2018;
+nowx = 2038;
+console.log(nowx)
+console.log(now - 1991 > now - 2018);
+
